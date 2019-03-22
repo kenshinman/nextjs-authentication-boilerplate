df217efce49baf914976cfb609cb12bf5f29d54b
Kehinde Orilogbon <archkenny@yahoo.com>
2019-03-22 11:04:31 +0100

    integreated bootstrap

--- components/Layout.js
+++ components/Layout.js
@@ -2,38 +2,41 @@
 import Head from "next/head";
 import { connect } from "react-redux";
 import actions from "../redux/actions";
-import {
-  AppBar,
-  Toolbar,
-  Typography,
-  Button,
-  IconButton
-} from "@material-ui/core";
-import MenuIcon  from "@material-ui/icons/Menu";
+import { Nav, Navbar, NavDropdown } from "react-bootstrap";
 
-const Layout = ({ children, title}) => (
+const Layout = ({ children, title, isAuthenticated, deauthenticate }) => (
   <div>
     <Head>
       <title>{title}</title>
       <meta charSet="utf-8" />
+      {(isAuthenticated, deauthenticate)}
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       {/* <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.2/css/bulma.min.css" /> */}
+      <link rel="stylesheet" href="static/css/bootstrap.min.css" />
     </Head>
     <div className="">
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            className={"classes.menuButton"}
-            color="inherit"
-            aria-label="Menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" color="inherit" className={"classes.grow"}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#home">Web App</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/whoami">Profile</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      ;
       {/* <ul>
         <Link href="/"><a>Home</a></Link>
         {!isAuthenticated && <Link href="/signin"><a>Sign In</a></Link>}
